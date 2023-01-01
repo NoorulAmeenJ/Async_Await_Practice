7c0b2567e441a4f43958531766e301fe89cde37f
NoorulAmeenJ <noorularafat786@gmail.com>
2023-01-01 19:15:23 +0530

    day -1 task going on

--- script.js
+++ script.js
@@ -1,69 +1,128 @@
-// function welcome(name,callback){
-//     console.log(`hiii ${name} welcome function `)
-//     callback(name)
-// }
-// function orderfromguest(name,callback) {
-//     console.log(`hiii ${name} from order function `)
-//     callback(name)
-// }
-// function dispatch(name,callback){
-//     console.log(`hiii ${name} dispatch function `)
-//     callback(name)
-// }
-// function greeting(name){
-//     console.log(`hiii ${name} greeting function `) 
-// }
-// function registerin(name,callback){
-//     console.log(`hiii ${name} registerin function `)
-//     setTimeout(   ()=>{ callback(name)},2000   )
-// }
+// // function welcome(name,callback){
+// //     console.log(`hiii ${name} welcome function `)
+// //     callback(name)
+// // }
+// // function orderfromguest(name,callback) {
+// //     console.log(`hiii ${name} from order function `)
+// //     callback(name)
+// // }
+// // function dispatch(name,callback){
+// //     console.log(`hiii ${name} dispatch function `)
+// //     callback(name)
+// // }
+// // function greeting(name){
+// //     console.log(`hiii ${name} greeting function `) 
+// // }
+// // function registerin(name,callback){
+// //     console.log(`hiii ${name} registerin function `)
+// //     setTimeout(   ()=>{ callback(name)},2000   )
+// // }
    
-// registerin("sanjay",(name)=>{
-//     welcome("noorul",(name)=>{
-//         orderfromguest("arafat",(name)=>{
-//             dispatch("jabar",(name)=>
-//             greeting("aakifah")  
-//              )
+// // registerin("sanjay",(name)=>{
+// //     welcome("noorul",(name)=>{
+// //         orderfromguest("arafat",(name)=>{
+// //             dispatch("jabar",(name)=>
+// //             greeting("aakifah")  
+// //              )
+// //     })
+// //     })
+// // })
+
+// // person has to be register
+// // pick up a projects
+// // number of products 
+// // dispatched
+// // delivery made
+
+// function register(payer,callback){
+//     console.log(`hiii ${payer} registered successfully`)
+//     callback(payer)
+// }
+// function pickup(product,callback){
+//     console.log(`you    picked up a  ${product} `)
+//     callback()
+// }
+
+// function number(engal,callback){
+//     console.log(`hiii  ${engal} product picked up successfully`)
+//     callback()
+
+// }
+// function dispatch(yes,callback){
+//     console.log(`your order is dispatched`)
+//     callback(yes)
+// }
+// function delivery(del){
+//     console.log(`your order is ${del}`)
+// }
+
+// register("noorul",()=>{
+//     pickup("tv",()=>{
+//         number("1",()=>{
+//             dispatch("yes",()=>{
+//                 delivery("delivered")
+//         })
 //     })
-//     })
+//   })
 // })
+// // -----------day 2 --------------------------------------------------------
 
-// person has to be register
-// pick up a projects
-// number of products 
-// dispatched
-// delivery made
+// function first(aaki){
+//     console.log("first")
+//     setTimeout(second,1000)
+//     third()
+//     fourth()
+    
+// }
+// function second(aaki){
+//     console.log("second")
+// }
+// function third(aaki){
+//     console.log("third")
+// }
+// function fourth(aaki){
+//     console.log("fourth")
+// }
+// first()
 
-function register(payer,callback){
-    console.log(`hiii ${payer} registered successfully`)
-    callback(payer)
+// answer
+// first
+// third
+// fourth
+// second
+
+function first(callback){
+    console.log("first")
+   setTimeout( callback, 3000)
+
 }
-function pickup(product,callback){
-    console.log(`you    picked up a  ${product} `)
+function second(callback){
+    console.log("second")
     callback()
 }
-
-function number(engal,callback){
-    console.log(`hiii  ${engal} product picked up successfully`)
-    callback()
-
+function third(callback){
+    console.log("third")
+   setTimeout( callback,5000)
 }
-function dispatch(yes,callback){
-    console.log(`your order is dispatched`)
-    callback(yes)
-}
-function delivery(del){
-    console.log(`your order is ${del}`)
+function fourth(callback){
+    console.log("fourth")
 }
 
-register("noorul",()=>{
-    pickup("tv",()=>{
-        number("1",()=>{
-            dispatch("yes",()=>{
-                delivery("delivered")
-        })
+
+first(()=>{
+       second(()=>{
+        third(()=>{
+           fourth()
+      })
     })
-  })
 })
+
+// first
+// second
+// third
+// fourth
+
+
+
 
 
