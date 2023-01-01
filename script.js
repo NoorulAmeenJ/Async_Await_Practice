7cd97d22d49116fd2e4f99bf9c32f66d6cf60621
NoorulAmeenJ <noorularafat786@gmail.com>
2023-01-01 12:27:14 +0530

    callback function completed

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
-function welcome(name,callback){
-    console.log(`hiii ${name} welcome function `)
-    callback(name)
+// function welcome(name,callback){
+//     console.log(`hiii ${name} welcome function `)
+//     callback(name)
+// }
+// function orderfromguest(name,callback) {
+//     console.log(`hiii ${name} from order function `)
+//     callback(name)
+// }
+// function dispatch(name,callback){
+//     console.log(`hiii ${name} dispatch function `)
+//     callback(name)
+// }
+// function greeting(name){
+//     console.log(`hiii ${name} greeting function `) 
+// }
+// function registerin(name,callback){
+//     console.log(`hiii ${name} registerin function `)
+//     setTimeout(   ()=>{ callback(name)},2000   )
+// }
+   
+// registerin("sanjay",(name)=>{
+//     welcome("noorul",(name)=>{
+//         orderfromguest("arafat",(name)=>{
+//             dispatch("jabar",(name)=>
+//             greeting("aakifah")  
+//              )
+//     })
+//     })
+// })
+
+// person has to be register
+// pick up a projects
+// number of products 
+// dispatched
+// delivery made
+
+function register(payer,callback){
+    console.log(`hiii ${payer} registered successfully`)
+    callback(payer)
 }
-function orderfromguest(name,callback) {
-    console.log(`hiii ${name} from order function `)
-    callback(name)
+function pickup(product,callback){
+    console.log(`you    picked up a  ${product} `)
+    callback()
 }
-function dispatch(name,callback){
-    console.log(`hiii ${name} dispatch function `)
-    callback(name)
+function number(engal,callback){
+    console.log(`hiii  ${engal} product picked up successfully`)
+    callback()
+
 }
-function greeting(name){
-    console.log(`hiii ${name} greeting function `) 
+function dispatch(yes,callback){
+    console.log(`your order is dispatched`)
+    callback(yes)
 }
-function registerin(name,callback){
-    console.log(`hiii ${name} registerin function `)
-    callback(name)
+function delivery(del){
+    console.log(`your order is ${del}`)
 }
-registerin("sanjay",(name)=>{
-    welcome(name,(name)=>{
-        orderfromguest(name,(name)=>{
-            dispatch(name,()=>
-            greeting()  
-             )
+
+register("noorul",()=>{
+    pickup("tv",()=>{
+        number("1",()=>{
+            dispatch("yes",()=>{
+                delivery("delivered")
+        })
     })
-    })
+})
+
 })
 
 
